9e8fa0ee02cbdc027d0b7bdf0b4ba933c2bf82c2
이현 <won983212@naver.com>
2020-11-22 17:19:13 +0900

    SplashScreen 분리

---
+++ Minecraft Launcher for Server/Components/SplashScreen.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Minecraft_Launcher_for_Server.Components
+{
+    /// <summary>
+    /// SplashScreen.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SplashScreen : UserControl
+    {
+        public static readonly DependencyProperty IsActiveProperty = DependencyProperty.Register("IsActive", 
+            typeof(bool), typeof(SplashScreen), new FrameworkPropertyMetadata(false));
+
+        public bool IsActive
+        {
+            get => (bool)GetValue(IsActiveProperty);
+            set { SetValue(IsActiveProperty, value); }
+        }
+
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
